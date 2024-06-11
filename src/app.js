bd86e9284e3eb55074a9d72460d2660e60e9772c
henryrothbard <henryrothbard24@gmail.com>
2024-06-11 12:00:48 -0400

    commit

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(logger('dev'));
-app.use(cors());
+app.use(cors({exposedHeaders: ['Authorization']}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
